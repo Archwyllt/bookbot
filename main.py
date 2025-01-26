9abd8790fcee3f0a03b02663b18f6e15b362c88c
Archwyllt <michaud.franc@gmail.com>
2025-01-25 22:27:01 -0500

    main.py functionnal added

---
+++ main.py
@@ -0,0 +1,28 @@
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    return file_contents
+
+def number_length(book_str):
+    return len(book_str.split())
+
+def char_count(book_str):
+    count = {}
+
+    for char in book_str.lower():
+        if char in count:
+            count[char] += 1
+        else: 
+            count[char] = 1
+    return count
+
+def report(book_str, char_count):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{number_length(book_str)} words found in the document\n")
+
+    for letter in "abcdefghijklmnopqrstuvwxz":
+        print(f"The '{letter}' character was found {char_count[letter]} times")
+    
+    print("--- End report ---")
+
+report(main(), char_count(main()))
