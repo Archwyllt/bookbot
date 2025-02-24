e5bd913017e398dcaf7d22f1b92f9820342b8106
Archwyllt <michaud.franc@gmail.com>
2025-02-24 15:54:13 -0500

    last update with modification

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
-from stats import get_num_words
+from stats import get_num_words, char_count, sorted_list
+import sys
 
 def main():
-    with open("books/frankenstein.txt") as f:
+    if len(sys.argv) != 2:
+        # If not, print usage message and exit with error code
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    pathway = sys.argv[1]
+    book_str = get_book_text(pathway)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {pathway}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(book_str)} total words")
+    print("--------- Character Count -------")
+
+    for char_dict in sorted_list(char_count(book_str)):
+        # Each dictionary has one key-value pair
+        # Extract the character and count from the dictionary
+        char = list(char_dict.keys())[0]
+        count = char_dict[char]
+        
+        # Skip non-alphabetical characters
+        if not char.isalpha():
+            continue
+            
+        # Print the character and its count in a formatted string
+        print(f"'{char}: {count}")
+    
+    print("============= END ===============")
+
+def get_book_text(pathway):
+    with open(pathway) as f:
         file_contents = f.read()
     return file_contents
 
-def char_count(book_str):
-    count = {}
-
-    for char in book_str.lower():
-        if char in count:
-            count[char] += 1
-        else: 
-            count[char] = 1
-    return count
-
-def report(book_str, char_count):
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{get_num_words(book_str)} words found in the document\n")
-
-    for letter in "abcdefghijklmnopqrstuvwxz":
-        print(f"The '{letter}' character was found {char_count[letter]} times")
-    
-    print("--- End report ---")
 
 if __name__ == "__main__":
-    report(main(), char_count(main()))
+    main()
--- stats.py
+++ stats.py
@@ -1,2 +1,24 @@
 def get_num_words(book_str):
     return len(book_str.split())
+
+def char_count(book_str):
+    count = {}
+
+    for char in book_str.lower():
+        if char in count:
+            count[char] += 1
+        else: 
+            count[char] = 1
+    return count
+
+def sorted_list(char_counts):
+    result = []
+    
+    for char, count in char_counts.items():
+        char_dict = {char: count}
+        
+        result.append(char_dict)
+
+    result.sort(key=lambda x: list(x.values())[0], reverse=True)
+    
+    return result
